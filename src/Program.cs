07f170bfadf4bd118087fa98bd154696a3edf3b3
MarcosHCK <marcoshck16@gmail.com>
2023-11-04 12:05:55 -0400

    Correcting solution and application

--- src/Program.cs
+++ src/Program.cs
@@ -1,27 +1,41 @@
-var builder = WebApplication.CreateBuilder(args);
+/* Copyright (c) 2023-2025
+ * This file is part of sep3cs.
+ *
+ * sep3cs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sep3cs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sep3cs. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace DataClash
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  class Program
+    {
+      public static int Main (string[] args)
+        {
+          var builder = WebApplication.CreateBuilder (args);
+
+          builder.Services.AddControllersWithViews ();
+
+          var app = builder.Build ();
+
+          if (app.Environment.IsDevelopment () == false)
+            app.UseHsts ();
+
+          app.UseHttpsRedirection ();
+          app.UseStaticFiles ();
+          app.UseRouting ();
+          app.MapControllerRoute (name : "default", pattern : "{controller}/{action=Index}/{id?}");
+          app.MapFallbackToFile ("index.html");;
+          app.Run ();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");;
-
-app.Run();
