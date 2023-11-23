203d0c2254b95c275e84192b418e4d1708deb61e
MarcosHCK <marcoshck16@gmail.com>
2023-11-23 12:51:12 -0500

    Identity profile page

--- src/WebAPI/ClientApp/src/components/Profile/ProfileIdentity.js
+++ src/WebAPI/ClientApp/src/components/Profile/ProfileIdentity.js
@@ -14,13 +14,45 @@
  * You should have received a copy of the GNU General Public License
  * along with sep3cs. If not, see <http://www.gnu.org/licenses/>.
  */
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap'
 import { ProfilePage } from './ProfilePage'
-import React from 'react'
+import { useErrorReporter } from '../ErrorReporter'
+import React, { useState } from 'react'
 
-export function ProfileIdentity ()
+export function ProfileIdentity (props)
 {
+  const { userProfile } = props
+  const [ userName, setUserName ] = useState (userProfile.name)
+  const [ userNick, setUserNick ] = useState (userProfile.nickname)
+  const [ userEmail, setUserEmail ] = useState (userProfile.email)
+  const errorReporter = useErrorReporter ()
+
+  const onSubmit = (e) =>
+    {
+      e.preventDefault ()
+
+      if (userProfile.name !== userName)
+        {
+          errorReporter (new Error ('Unimplemented'))
+        }
+    }
+
   return (
     <ProfilePage title='Identity'>
-      <p>asdasd</p>
+      <Form onSubmit={(e) => { onSubmit (e) }}>
+        <FormGroup floating>
+          <Input id='identity-input-useremail' type='text' onChange={(e) => setUserEmail (e.target.value)} value={userEmail} disabled/>
+          <Label for='identity-input-useremail'>Email</Label>
+        </FormGroup>
+        <FormGroup floating>
+          <Input id='identity-input-username' type='text' onChange={(e) => setUserName (e.target.value)} value={userName} />
+          <Label for='identity-input-username'>Username</Label>
+        </FormGroup>
+        <FormGroup floating>
+          <Input id='identity-input-usernick' type='text' onChange={(e) => setUserNick (e.target.value)} value={userNick} />
+          <Label for='identity-input-usernick'>Nickname</Label>
+        </FormGroup>
+        <Button color='primary'>Update</Button>
+      </Form>
     </ProfilePage>)
 }
