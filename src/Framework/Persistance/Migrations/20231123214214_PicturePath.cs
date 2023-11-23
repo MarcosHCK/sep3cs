ccd62c824958bd3bde050c5dcf1daa9ace9b7860
MarcosHCK <marcoshck16@gmail.com>
2023-11-23 16:39:27 -0500

    Fixed cards seeding

---
+++ src/Framework/Persistance/Migrations/20231123214214_PicturePath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Framework.Migrations
+{
+    /// <inheritdoc />
+    public partial class PicturePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagePath",
+                table: "Cards",
+                newName: "Picture");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Picture",
+                table: "Cards",
+                newName: "ImagePath");
+        }
+    }
+}
