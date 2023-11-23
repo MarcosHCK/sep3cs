1dabb39321d1b272f1e97c99e51c1ee3b0131150
MarcosHCK <marcoshck16@gmail.com>
2023-11-22 19:17:55 -0500

    Submit support (not working due to missing userId)

--- src/WebAPI/ClientApp/src/components/ErrorReporter.js
+++ src/WebAPI/ClientApp/src/components/ErrorReporter.js
@@ -15,6 +15,7 @@
  * along with sep3cs. If not, see <http://www.gnu.org/licenses/>.
  */
 import { Button, Form, Input, Modal, ModalBody, ModalHeader } from 'reactstrap'
+import { CreateErrorReportCommand, ReportClient } from '../webApiClient.ts'
 import React, { createContext, useContext, useState } from 'react'
 const thisContext = createContext ()
 
@@ -23,11 +24,51 @@ export function useErrorReporter ()
   return useContext (thisContext)
 }
 
+export function ErrorReporterForm (props)
+{
+  const { hasNext, onPopAll, onPopError } = props
+  const [ description, setDescription ] = useState ('')
+  const [ reportClient ] = useState (new ReportClient ())
+
+  const onSubmit = (e) =>
+    {
+      e.preventDefault ()
+
+      const command = new CreateErrorReportCommand ()
+
+      command.userId = ''
+      command.message = description
+
+      // Exception inside exception handler, uff...
+      reportClient.create (command)
+
+      setDescription ('')
+      onPopError (false)
+    }
+
+  return (
+    <Form onSubmit={(e) => onSubmit (e)}>
+      <Input placeholder='Please let us know what you were doing when the error showed up'
+        type='textarea' value={description} onChange={(e) => setDescription (e.target.value)} />
+      <div className='d-flex gap-2 justify-content-end mt-2'>
+        {
+          !hasNext
+            ? (<Button onClick={() => { onPopAll () }}>Close</Button>)
+            : (<>
+              <Button onClick={() => { onPopError () }}>Show next error</Button>
+              <Button onClick={() => { onPopAll () }}>Clear all errors</Button>
+            </>)
+        }
+        <Button>Submit</Button>
+      </div>
+    </Form>)
+}
+
 export function ErrorReporterProvider (props)
 {
   const [ isOpen, setIsOpen ] = useState (false)
   const [ stack, setStack ] = useState ([])
-console.log (stack)
+
   return (
     <thisContext.Provider
       value={(error) => { setStack ([...stack, error]); setIsOpen (true) }}>
@@ -37,7 +78,11 @@ console.log (stack)
         isOpen={isOpen}
         onClosed={() => { setIsOpen (false); }}
         onPopAll={() => { setIsOpen (false); setStack ([]) }}
-        onPopError={() => { setStack (stack.slice (0, -1)); }} />
+        onPopError={(alsoClose = isOpen) =>
+          {
+            setIsOpen (alsoClose)
+            setStack (stack.slice (0, -1))
+          }} />
       { props.children }
     </thisContext.Provider>
   )
@@ -54,23 +99,8 @@ export function ErrorReporterWidget (props)
       </ModalHeader>
 
       <ModalBody>
-
         <p>{ !errorObject ? null : errorObject.message }</p>
-
-        <Form>
-          <Input type='textarea' placeholder='Please let us know what you were doing when the error showed up' />
-          <div className='d-flex gap-2 justify-content-end mt-2'>
-        {
-          !hasNext
-          ? (<Button onClick={() => onPopAll ()}>Close</Button>)
-          : (<>
-              <Button onClick={() => onPopError ()}>Show next error</Button>
-              <Button onClick={() => onPopAll ()}>Clear all errors</Button>
-            </>)
-        }
-            <Button>Submit</Button>
-          </div>
-        </Form>
+        <ErrorReporterForm hasNext={hasNext} onPopAll={onPopAll} onPopError={onPopError} />
       </ModalBody>
     </Modal>)
 }
