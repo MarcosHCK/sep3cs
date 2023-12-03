84eac1c75d694926f4f162905c672807cf54a4c8
MarcosHCK <marcoshck16@gmail.com>
2023-12-03 10:19:35 -0500

    Fixing matches component

--- src/WebAPI/ClientApp/src/components/Matches.js
+++ src/WebAPI/ClientApp/src/components/Matches.js
@@ -14,10 +14,12 @@
  * You should have received a copy of the GNU General Public License
  * along with sep3cs. If not, see <http://www.gnu.org/licenses/>.
  */
-import { Button, Table,Input, Label } from 'reactstrap'
+import { Button, Table, Input } from 'reactstrap'
 import { CreateMatchCommand } from '../webApiClient.ts'
 import { DateTime } from './DateTime'
+import { MatchClient } from '../webApiClient.ts'
 import { Pager } from './Pager'
+import { PlayerInput } from './PlayerInput'
 import { TimeSpan } from './TimeSpan'
 import { UpdateMatchCommand } from '../webApiClient.ts'
 import { useAuthorize } from '../services/AuthorizeProvider'
@@ -25,64 +27,44 @@ import { useErrorReporter } from './ErrorReporter'
 import { useParams } from 'react-router-dom'
 import { UserRoles } from '../services/AuthorizeConstants'
 import { WaitSpinner } from './WaitSpinner'
-import { MatchClient } from '../webApiClient.ts'
 import React, { useEffect, useState } from 'react'
-
 
 export function Matches ()
 {
   const { initialPage } = useParams ()
+  const { isAuthorized, inRole }= useAuthorize ()
   const [ activePage, setActivePage ] = useState (initialPage ? initialPage : 0)
   const [ hasNextPage, setHasNextPage ] = useState (false)
   const [ hasPreviousPage, setHasPreviousPage ] = useState (false)
-  const { isAuthorized, inRole }= useAuthorize ()
   const [ isLoading, setIsLoading ] = useState (false)
   const [ items, setItems ] = useState (undefined)
-  const [ totalPages, setTotalPages ] = useState (0)
   const [ matchClient ] = useState (new MatchClient ())
+  const [ totalPages, setTotalPages ] = useState (0)
+  const [ matchWinner, setMatchWinner ] = useState (0)
+  const [ matchLoser, setMatchLoser ] = useState (0)
+  const [ matchBeginDate, setMatchBeginDate ] = useState (new Date ())
+  const [ matchDuration, setMatchDuration ] = useState ('00:00:01')
   const errorReporter = useErrorReporter ()
-
-  
 
   const pageSize = 10
   const visibleIndices = 5
-  var mydate = new Date()
-  var winner = 1
-  var looser = 2
-  var dur = "00:00:01"
-  function SetWinner(val)
-  {
-    winner = val
-  }
-  function SetLooser(val)
-  {
-    looser = val
-  }
-  function SetDate(val) 
-  { 
-    mydate = val  
-  }
-  function SetDuration(val) 
-  { 
-    dur = val  
-  }
+
   const addMatch = async () =>
     {
       const data = new CreateMatchCommand ()
-      data.winnerPlayerId = winner
-      data.looserPlayerId = looser
-      data.beginDate = mydate
-      data.duration = dur
-      try
-      {
+  
+      data.winnerPlayerId = matchWinner
+      data.looserPlayerId = matchLoser
+      data.beginDate = matchBeginDate
+      data.duration = matchDuration
+
+      try {
         await matchClient.create (data)
         setActivePage (-1)
-      }
-      catch(error)
-      {
-        errorReporter(error)
-      }
-      
+      } catch (error)
+        {
+          errorReporter (error)
+        }
     }
 
   const removeMatch = async (item) =>
@@ -169,10 +151,8 @@ export function Matches ()
 
   return (
     isLoading || !isAuthorized
-      ? (<WaitSpinner/>)
-    : (
-      <>
-        
+    ? <WaitSpinner/>
+    : <>
         <div className='d-flex justify-content-center'>
           <Pager
             activePage={activePage}
@@ -182,91 +162,70 @@ export function Matches ()
             totalPages={totalPages}
             visibleIndices={visibleIndices} />
         </div>
-        
-        <div>
-        <h1>Matches</h1>
-          <Table border='1'>
-            <thead>
-              <tr>
-                <th>{'Winner player'}</th>
-                <th>{'Looser player'}</th>
-                <th>{'Begin date'}</th>
-                <th>{'Duration'}</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
+        <Table border='1'>
+          <thead>
+            <tr>
+              <th>{'Winner player'}</th>
+              <th>{'Loser player'}</th>
+              <th>{'Begin date'}</th>
+              <th>{'Duration'}</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
         { (items ?? []).map ((item, index) => (
-              <tr key={`body${index}`}>
-                <td>
-                  <p>{ item.winnerPlayerId }</p>
-                </td>
-                <td>
-                  <p>{ item.looserPlayerId }</p>
-                </td>
-                <td>
-                  <DateTime
-                    defaultValue={item.beginDate}
-                    readOnly={true} />
-                </td>
-                <td>
-                  <TimeSpan
-                    defaultValue={item.duration}
-                    onChanged={(span) => { item.duration = span; updateMatch (item) }}
-                    readOnly={!inRole[UserRoles.Administrator]} />
-                </td>
-        {
-          (!inRole[UserRoles.Administrator])
-          ? (<td />)
-          : (
-                <td>
-                  <Button color='primary' onClick={() => removeMatch (item)} close />
-                </td>)
-        }
-              </tr>))
-        }
-            </tbody>
-            <tfoot>
-        {
-          (!inRole[UserRoles.Administrator])
-          ? (<tr />)
-          : (
-              <tr key='footer0' striped bordered dark color = 'blue'>
-                
-                
-                <td>
-                  <Input
-                    type='number'
-                    onChange={(e) => SetWinner (e.target.value)}/>
-                </td>
-                <td>
-                  <Input
-                    type='number'
-                    onChange={(e) => SetLooser (e.target.value)}/>
-                </td>
-                
-                <td>
-                <DateTime
-                    defaultValue={new Date()}
-                    onChanged={(date) => { SetDate(date) }}
-                    readOnly={!inRole[UserRoles.Administrator]} />
-                </td>
-
+            <tr key={`body${index}`}>
+              <td>
+                <Input defaultValue={ item.winnerPlayerId } disabled />
+              </td>
+              <td>
+                <Input defaultValue={ item.looserPlayerId } disabled />
+              </td>
+              <td>
+                <DateTime defaultValue={item.beginDate} readOnly />
+              </td>
+              <td>
                 <TimeSpan
-                    defaultValue='00:00:01'
-                    onChanged={(span) => { SetDuration (span) }}
-                    readOnly={!inRole[UserRoles.Administrator]} />
-                <td>
-                  <Button color='primary' onClick={() => addMatch ()}>+</Button>
-                </td>
-                
-              </tr>
-              )
-        }
-         </tfoot>
-            
-           
-          </Table>
-        </div>
-      </>))
+                  defaultValue={item.duration}
+                  onChanged={(span) => { item.duration = span; updateMatch (item) }}
+                  readOnly={!inRole[UserRoles.Administrator]} />
+              </td>
+          { (!inRole[UserRoles.Administrator])
+            ? <td />
+            : <td>
+                <Button color='primary' onClick={() => removeMatch (item)} close />
+              </td>}
+            </tr>))}
+          </tbody>
+          <tfoot>
+        { (!inRole[UserRoles.Administrator])
+          ? <tr />
+          : <tr key='footer0'>
+              <td>
+                <Input
+                  type='number'
+                  onChange={e => setMatchWinner (e.target.value)}
+                  value={matchWinner} />
+              </td>
+              <td>
+                <Input
+                  type='number'
+                  onChange={e => setMatchLoser (e.target.value)}
+                  value={matchLoser} />
+              </td>
+              <td>
+                <DateTime
+                  defaultValue={matchBeginDate}
+                  onChanged={date => setMatchBeginDate (date)} />
+              </td>
+                <TimeSpan
+                  defaultValue={matchDuration}
+                  onChanged={span => setMatchDuration (span)} />
+              <td>
+                <Button color='primary' onClick={() => addMatch ()}>+</Button>
+              </td>
+            </tr>}
+          </tfoot>
+        </Table>
+      </>)
 }
